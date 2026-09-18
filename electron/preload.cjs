cb167b066cf064aef0e640e9b8194ebd15bf0448
iptvfp25 <iptv.fp25@gmail.com>
2026-09-18 19:30:47 +0200

    Expose window control IPC calls for custom title bar buttons

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,6 +1,9 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronApp', {
   platform: process.platform,
   isElectron: true,
+  minimizeWindow: () => ipcRenderer.send('window-minimize'),
+  maximizeWindow: () => ipcRenderer.send('window-maximize'),
+  closeWindow: () => ipcRenderer.send('window-close'),
 });
